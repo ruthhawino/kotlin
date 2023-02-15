9944df041f22d7d6cb072ad1f2084e8f89deed68
ruthhawino <awinoruthh@gmail.com>
2023-02-15 23:00:40 +0300

    created file that returns and print result

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+var school = "AkiraChix"
+ println(school[1]+""+school[3]+""+school[4])
+statement("Ruth" ,"19")
+ length("42")
+name("")
+}
+fun statement(fullname:String,age:String){
+ println("Hi," + " " + "My name is" + " "+fullname+ " "+"and I am"+" " +age+"Years Old")
+
+}
+fun length(Int:String){
+ println(Int + " " +"cm" )
+}
+fun name(ruth:String){
+ println("That's Me")
+
+}
